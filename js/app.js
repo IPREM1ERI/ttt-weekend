110fe3f7818d9df455af7fce8cfd037af7873b94
davidv <davidvazquezgit@gmail.com>
2022-09-02 08:51:02 -0700

    create const variable, work on render func

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [[1,1,1], [-1,-1,-1]]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -9,6 +9,7 @@ let winner, turn, board
 /*------------------------ Cached Element References ------------------------*/
 
 const squareEls = document.querySelectorAll("div.board-space")
+//console.log(squareEls)
 const messageEls = document.querySelector("#message")
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -21,5 +22,17 @@ function init() {
 board = [null,null,null,null,null,null,null,null,null]
 turn = 1
 winner = null
+render()
+}
+function render() {
+  board.forEach((boardSpace, idx) => {
+    boardSpace = squareEls[idx]
+    console.log(boardSpace)
+    
+  })
+  // if (winner === null) {
+  //   `It is player ${turn} turn`
+  // }
+  
 }
 
