c94811be5f0b1d9fc89081dd611c2a7041f2c1bf
davidv <davidvazquezgit@gmail.com>
2022-09-02 07:45:20 -0700

    create chache el, init function

--- js/app.js
+++ js/app.js
@@ -3,16 +3,23 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let winner, turn, board
 
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+const squareEls = document.querySelectorAll("div.board-space")
+const messageEls = document.querySelector("#message")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+function init() {
+board = [null,null,null,null,null,null,null,null,null]
+turn = 1
+winner = null
+}
 
